552269542939bede2cf84a3fa7f38d8677b776c9
SupriyaPoojari <168285714+SupriyaPoojari@users.noreply.github.com>
2026-03-08 12:23:37 -0700

    Update jen.java

--- jen.java
+++ jen.java
@@ -2,5 +2,6 @@ public class jen{
     public static void main(String[] args){
         System.out.println("Hello Jenkins");
         System.out.println("This is the 4th question of Assignment 2");
+         System.out.println("Hello from Poll SCM Trigger");
     }
 }
