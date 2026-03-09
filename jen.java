8840ab96e0e8e5b5b9d8829324e86b027bd07f79
SupriyaPoojari <168285714+SupriyaPoojari@users.noreply.github.com>
2026-03-08 21:02:42 -0700

    Update jen.java

--- jen.java
+++ jen.java
@@ -1,7 +1,7 @@
 public class jen{
     public static void main(String[] args){
-        System.out.println("Hello Jenkins from Pipeline Job !!");
-        System.out.println("This is the 8th question of Assignment 2");
+        System.out.println("Hello Jenkins  !!");
+        System.out.println("This is the 4th question of Assignment 2");
          
          
     }
