f7684c9f8b5a1abc7d7b0024d6b8a6b924c9661f
SupriyaPoojari <168285714+SupriyaPoojari@users.noreply.github.com>
2026-03-08 17:39:16 -0700

    Update jen.java

--- jen.java
+++ jen.java
@@ -1,8 +1,8 @@
 public class jen{
     public static void main(String[] args){
-        System.out.println("Hello Jenkins");
-        System.out.println("This is the 4th question of Assignment 2");
+        System.out.println("Hello Jenkins from Github SCM");
+        System.out.println("This is the 6th question of Assignment 2");
          
-         System.out.println("This line is for Poll SCM");
+         
     }
 }
